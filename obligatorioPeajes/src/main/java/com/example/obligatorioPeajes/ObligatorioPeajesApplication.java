385d10fc9c9e5f57ebb9ba98ae04031dbd8253e3
Matias Jimenez Salice <matias.j.salice@gmail.com>
2025-11-15 12:30:28 -0300

    Agregar la estructura inicial del proyecto y los archivos de configuración para ObligatorioPeajes

---
+++ obligatorioPeajes/src/main/java/com/example/obligatorioPeajes/ObligatorioPeajesApplication.java
@@ -0,0 +1,13 @@
+package com.example.obligatorioPeajes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObligatorioPeajesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObligatorioPeajesApplication.class, args);
+	}
+
+}
---
+++ obligatorioPeajes/src/test/java/com/example/obligatorioPeajes/ObligatorioPeajesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.obligatorioPeajes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObligatorioPeajesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
